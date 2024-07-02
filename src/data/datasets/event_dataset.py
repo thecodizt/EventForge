535cef28845547ffd58360f2cf4c5fc29c406c55
Dev <dev@thecodizt.com>
2024-07-02 16:57:57 +0530

    UPDATE: Handle all input fields

--- src/data/datasets/event_dataset.py
+++ src/data/datasets/event_dataset.py
@@ -1,22 +1,24 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
 import pandas as pd
 import json
-
+from torch.nn.utils.rnn import pad_sequence
 from constants import ROOT_DIR
 
 class EventDataset(Dataset):
-    def __init__(self, csv_file, sequence_length, vocab=None):
+    def __init__(self, csv_file, sequence_length):
         self.data = pd.read_csv(f'{ROOT_DIR}/data/processed' + csv_file)
         self.sequence_length = sequence_length
-        self.vocab = vocab if vocab else self.build_vocab()
+        self.vocab = self.build_vocab()
         self.vocab_size = len(self.vocab)
+        self.index_to_token = {v: k for k, v in self.vocab.items()}
 
     def build_vocab(self):
         vocab = {'<PAD>': 0, '<UNK>': 1}
         for _, row in self.data.iterrows():
             vocab.setdefault(row['event_type'], len(vocab))
+            vocab.setdefault(row['agent_id'], len(vocab))
             context = json.loads(row['context'])
             for key, value in context.items():
                 vocab.setdefault(key, len(vocab))
@@ -32,27 +34,36 @@ class EventDataset(Dataset):
 
         input_seq = []
         for _, event in sequence.iterrows():
-            event_encoding = [self.vocab.get(event['event_type'], self.vocab['<UNK>'])]
-            context = json.loads(event['context'])
-            for key, value in context.items():
-                event_encoding.append(self.vocab.get(key, self.vocab['<UNK>']))
-                event_encoding.append(self.vocab.get(str(value), self.vocab['<UNK>']))
-            
-            # Convert to one-hot encoding
-            one_hot = F.one_hot(torch.tensor(event_encoding), num_classes=self.vocab_size).float()
-            input_seq.append(one_hot.sum(dim=0))  # Sum to get a single vector per event
+            event_encoding = self.encode_event(event)
+            input_seq.append(event_encoding)
 
-        # Pad sequence if necessary
-        if len(input_seq) < self.sequence_length:
-            padding_length = self.sequence_length - len(input_seq)
-            input_seq.extend([torch.zeros(self.vocab_size)] * padding_length)
+        input_tensor = [torch.tensor(seq) for seq in input_seq]
+        target_tensor = self.encode_event(target)
 
-        input_tensor = torch.stack(input_seq)
-        
-        target_encoding = self.vocab.get(target['event_type'], self.vocab['<UNK>'])
-        target_tensor = torch.tensor(target_encoding, dtype=torch.long)
+        return input_tensor, target_tensor, sequence, target
 
-        return input_tensor, target_tensor
-    
-    def get_vocab_size(self):
-        return self.vocab_size
+    def encode_event(self, event):
+        event_encoding = [
+            self.vocab.get('cycle', self.vocab['<UNK>']),
+            self.vocab.get(event['event_type'], self.vocab['<UNK>']),
+            self.vocab.get(event['agent_id'], self.vocab['<UNK>'])
+        ]
+        context = json.loads(event['context'])
+        for key, value in context.items():
+            event_encoding.extend([
+                self.vocab.get(key, self.vocab['<UNK>']),
+                self.vocab.get(str(value), self.vocab['<UNK>'])
+            ])
+        return torch.tensor(event_encoding)
+
+    def decode_event(self, encoded_event):
+        decoded = {}
+        decoded['cycle'] = self.index_to_token[encoded_event[0].item()]
+        decoded['event_type'] = self.index_to_token[encoded_event[1].item()]
+        decoded['agent_id'] = self.index_to_token[encoded_event[2].item()]
+        decoded['context'] = {}
+        for i in range(3, len(encoded_event), 2):
+            key = self.index_to_token[encoded_event[i].item()]
+            value = self.index_to_token[encoded_event[i+1].item()]
+            decoded['context'][key] = value
+        return decoded
--- src/models/lstm_model.py
+++ src/models/lstm_model.py
@@ -1,27 +1,25 @@
 import torch
 import torch.nn as nn
-
-class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
-        super(LSTMModel, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
     
+class LSTMModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, num_layers, max_event_length):
+        super(LSTMModel, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+        self.max_event_length = max_event_length
+
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
-        return out
+        embedded = self.embedding(x)
+        lstm_out, _ = self.lstm(embedded)
+        output = self.fc(lstm_out[:, -1, :])
+        return output.view(-1, self.max_event_length, output.size(-1))
 
 def create_model(config, vocab_size):
     return LSTMModel(
-        input_size=vocab_size,
+        vocab_size=vocab_size,
+        embedding_dim=config['model']['embedding_dim'],
         hidden_size=config['model']['hidden_size'],
         num_layers=config['model']['num_layers'],
-        output_size=vocab_size
+        max_event_length=config['data']['max_event_length']
     )
